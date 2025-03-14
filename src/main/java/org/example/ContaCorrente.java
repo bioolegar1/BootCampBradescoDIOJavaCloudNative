2e4040ee3ca9b4c7c48741a563997ede2ecd5dbe
Vanderson H Santos <bioolegari@gmail.com>
2025-03-13 21:56:01 -0300

    classe conta corrente teste

---
+++ src/main/java/org/example/ContaCorrente.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class ContaCorrente {
+
+    private String nome;
+    private int numero;
+
+
+
+    public ContaCorrente() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+        System.out.println("Testando o codigo no java gradle");
+
+
+
+    }
+}
